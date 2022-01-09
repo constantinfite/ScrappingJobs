34fa7368fe562763855fa4b2c8864f2b34f17452
Constantin FIte <constantin.fite@gmail.com>
2022-01-09 22:51:32 +0100

    add sleep 10s to not be blocked

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from datetime import date
 import re
 import math
+import time
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
@@ -22,17 +23,18 @@ is_consulted = []
 def extract(job_elems):
     # For each job card we look for the informations
     for job_elem in job_elems:
-        job_titles.append(extract_job_title_indeed(job_elem))
-        company_names.append(extract_company_name_indeed(job_elem))
-        company_locations.append(extract_company_location_indeed(job_elem))
-        extract_dates.append(extract_extract_date())
-        job_links.append(extract_job_link_indeed(job_elem))
 
         try:
+            job_titles.append(extract_job_title_indeed(job_elem))
+            company_names.append(extract_company_name_indeed(job_elem))
+            company_locations.append(extract_company_location_indeed(job_elem))
+            extract_dates.append(extract_extract_date())
+            job_links.append(extract_job_link_indeed(job_elem))
             post_dates.append(extract_post_date(job_elem))
 
         except AttributeError:
-            post_dates.append("")
+            print("------------ Block -------------")
+            break
 
 
 def extract_job_title_indeed(element):
@@ -66,23 +68,24 @@ def extract_job_link_indeed(element):
     return link
 
 
-# def extract_number_pages(soup):
-#     raw_total_number_jobs = soup.find("div", class_="searchCountContainer").text
-#     raw_total_number_jobs = raw_total_number_jobs.replace(" ", "")
-#     regex = r"((\d+ )?\d+) emplois"
-#     total_job_array = re.findall(regex, raw_total_number_jobs)
-#     total_job = total_job_array[0][0]
-#     print("total_job", total_job)
-#     total_pages = math.ceil(int(total_job) / 15)
-#     print("total pages", total_pages)
+def extract_number_pages(soup):
+    raw_total_number_jobs = soup.find("div", class_="searchCountContainer").text
+    raw_total_number_jobs = raw_total_number_jobs.replace(" ", "")
+    regex = r"((\d+ )?\d+) emplois"
+    total_job_array = re.findall(regex, raw_total_number_jobs)
+    total_job = total_job_array[0][0]
+    print("total_job", total_job)
+    total_pages = math.ceil(int(total_job) / 15)
+    print("total pages", total_pages)
 
-    # return total_pages
+    return total_pages
 
 
 # Loop over pages
 def extract_all_pages(job, location, job_type):
-    url = f'https://fr.indeed.com/jobs?q={job}&l={location}&start=0'
-
+    url = f'https://fr.indeed.com/jobs?q={job}&l={location}&jt={job_type}&start=0'
+    print(url)
+    c = 0
     while True:
         r = requests.get(url, headers)
         soup = BeautifulSoup(r.content, 'html.parser')
@@ -92,9 +95,15 @@ def extract_all_pages(job, location, job_type):
 
         # If next button exist change url to the next url page
         try:
+
             url = "https://fr.indeed.com" + soup.find('a', {'aria-label': 'Suivant'}).get('href')
+            c += 1
+            print("Page number :", url)
+            time.sleep(10)
         except AttributeError:
             break
+
+    extract_number_pages(soup)
 
     indeed_dictionnary = {
         "Company": company_names,
@@ -106,4 +115,5 @@ def extract_all_pages(job, location, job_type):
     }
 
     df_extracted = pd.DataFrame(indeed_dictionnary)
+    print("Size of extracted df", df_extracted.shape)
     return df_extracted
